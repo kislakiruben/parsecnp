c6148d954db3c111f30c7d5dd3a131cce883d655
Ruben <ruben@kislaki.net>
2017-04-25 18:20:12 +0300

    reconfigured rollup to not use babelrc anymore

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,15 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-  entry: 'src/parsecnp.js',
-  format: 'umd',
-  plugins: [
-    babel({
-      exclude: 'node_modules/**'
-    })
-  ],
-  moduleName: 'ParseCNP',
-  dest: 'dist/parsecnp.min.js',
+    entry: 'src/parsecnp.js',
+    format: 'umd',
+    plugins: [
+        babel({
+            exclude: 'node_modules/**',
+            presets: [ 'es2015-rollup' ],
+            babelrc: false,
+        })
+    ],
+    moduleName: 'ParseCNP',
+    dest: 'dist/parsecnp.min.js',
 };
