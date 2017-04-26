65e8c6de08668813fa2997781057d7ffa8783621
Ruben <ruben@kislaki.net>
2017-04-26 20:42:17 +0300

    refactored regex method
    # Conflicts:
    #	src/Parser.js

--- src/Parser.js
+++ src/Parser.js
@@ -61,7 +61,9 @@ export default class Parser {
         });
     }
 
-    regex() {
-        return /(^[1-8])(\d{2})(0[1-9]|1[012])(0[1-9]|[12]\d|3[01])(\d{2})(\d{3})(\d)/.exec(this.raw);
+    regex(captureGroup) {
+        const match = /(^[1-8])(\d{2})(0[1-9]|1[012])(0[1-9]|[12]\d|3[01])(\d{2})(\d{3})(\d)/.exec(this.raw);
+
+        return match[captureGroup];
     }
 }
