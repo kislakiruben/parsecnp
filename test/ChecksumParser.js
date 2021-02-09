55a7f3ec4c061509606650d2e7cd1e0d7f2abc3f
YoussefKababe <youssef.kbe@gmail.com>
2021-02-09 11:16:36 +0100

    Keep validator value in case it's equal to 0

--- test/ChecksumParser.js
+++ test/ChecksumParser.js
@@ -6,6 +6,7 @@ describe(`checksumValidator`, () => {
     it(`should return 'true' for valid CNP`, () => {
         expect(checksumValidator('6130626527204', '4')).to.be.true;
         expect(checksumValidator('2941118271691', '1')).to.be.true;
+        expect(checksumValidator('1900803342580', '0')).to.be.true;
     });
 
     it(`should return 'false' for invalid CNP`, () => {
