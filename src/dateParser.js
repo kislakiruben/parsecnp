03ad4b6c79774466e4b494717dc4eb57c643f3e9
Andreas Hellström <andreash@gmail.com>
2021-12-07 12:00:27 +0100

    Fix correct century mapping for foreigner

--- src/dateParser.js
+++ src/dateParser.js
@@ -1,6 +1,10 @@
 import { isValidDate } from './utils';
 
 const dateParser = (sexCode, birthdate) => {
+    let year = birthdate.substr(0, 2);
+    const month = birthdate.substr(2, 2);
+    const day = birthdate.substr(4, 2);
+
     const centuryMapping = {
         1: '19',
         2: '19',
@@ -8,12 +12,11 @@ const dateParser = (sexCode, birthdate) => {
         4: '18',
         5: '20',
         6: '20',
-        7: '20',
-        8: '20',
+        7: year < 19 ? '20' : '19',
+        8: year < 19 ? '20' : '19',
     };
-    const month = birthdate.substr(2, 2);
-    const day = birthdate.substr(4, 2);
-    let year = `${centuryMapping[sexCode]}${birthdate.substr(0, 2)}`;
+
+    year = `${centuryMapping[sexCode]}${year}`;
     const date = new Date(year, month - 1, day);
 
     return isValidDate(year, month, day) ? date : new Date('');
