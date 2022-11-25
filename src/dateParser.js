3f1c75fe15fe8bab2797c91ec2eda98de419cfd4
Ruben <ruben@kislaki.net>
2022-11-25 23:02:57 +0200

    fixed how year is computed for residents

--- src/dateParser.js
+++ src/dateParser.js
@@ -1,22 +1,22 @@
 import { isValidDate } from './utils';
 
+const CENTURY_MAP = {
+    1: '19',
+    2: '19',
+    3: '18',
+    4: '18',
+    5: '20',
+    6: '20',
+};
 const dateParser = (sexCode, birthdate) => {
-    const centuryMapping = {
-        1: '19',
-        2: '19',
-        3: '18',
-        4: '18',
-        5: '20',
-        6: '20',
-        7: '20',
-        8: '20',
-    };
+    const year = birthdate.substr(0, 2);
     const month = birthdate.substr(2, 2);
     const day = birthdate.substr(4, 2);
-    const year = `${centuryMapping[sexCode]}${birthdate.substr(0, 2)}`;
-    const date = new Date(year, month - 1, day, 0, 0, 0, 0);
+    const isResident = sexCode === 7 || sexCode === 8;
+    const fullYear = `${isResident ? year < new Date().getFullYear().toString().substr(2, 2) ? 20 : 19 : CENTURY_MAP[sexCode]}${year}`;
+    const date = new Date(fullYear, month - 1, day, 0, 0, 0, 0);
 
-    return isValidDate(year, month, day) ? date : new Date('');
+    return isValidDate(fullYear, month, day) ? date : new Date('');
 };
 const dateValidator = (date) => !isNaN(date);
 
--- test/dateParser.test.js
+++ test/dateParser.test.js
@@ -3,11 +3,16 @@ import { expect } from 'chai';
 import { default as dateParser, dateValidator } from '../src/dateParser';
 
 describe(`date parser`, () => {
-    it(`should return create a correct date object`, () => {
+    it(`should create a correct date object`, () => {
         expect(dateParser(1, '880110')).to.equalDate(new Date(1988, 0, 10));
         expect(dateParser(5, '020202')).to.equalDate(new Date(2002, 1, 2));
         expect(dateParser(6, '120401')).to.equalDate(new Date(2012, 3, 1));
     });
+
+    it(`should correctly parse resident birthdate`, () => {
+        expect(dateParser(7, '120401')).to.equalDate(new Date(2012, 3, 1));
+        expect(dateParser(7, '880401')).to.equalDate(new Date(1988, 3, 1));
+    })
 });
 
 describe(`date validator`, () => {
