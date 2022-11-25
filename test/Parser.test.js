62482d06991c9b631a50b7a4370fe5e8286ed696
Ruben <ruben@kislaki.net>
2022-11-25 22:24:31 +0200

    fixed dates in unit tests

--- test/Parser.test.js
+++ test/Parser.test.js
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import sinon from 'sinon';
 
 import Parser from '../src/Parser';
 
@@ -17,13 +18,13 @@ describe(`main parser`, () => {
     });
 
     it(`should have getters`, () => {
-        const instance = new Parser('2121212261011');
+        const instance = new Parser('6121212261011');
 
         expect(instance.sex).to.be.equal('female');
-        expect(instance.birthdate).to.equalDate(new Date(1912, 11, 12));
+        expect(instance.birthdate).to.equalDate(new Date(2012, 11, 12));
         expect(instance.day).to.be.equal(12);
         expect(instance.month).to.be.equal(12);
-        expect(instance.year).to.be.equal(1912);
+        expect(instance.year).to.be.equal(2012);
         expect(instance.county).to.include({ code: '26', name: 'Mureș', ISO: 'MS' });
         expect(instance.serial).to.be.equal('101');
         expect(instance.checksum).to.be.equal('1');
@@ -31,9 +32,11 @@ describe(`main parser`, () => {
     });
 
     it(`should have methods`, () => {
-        const instance = new Parser('2121212261011');
+        const instance = new Parser('6121212261011');
+        const clock = sinon.useFakeTimers(new Date(2012, 11, 12).getTime())
 
-        expect(instance.toString()).to.equal('2121212261011');
-        expect(instance.toJSON()).to.deep.equal('{"birthdate":"1912-12-11T22:15:36.000Z","checksum":"1","county":{"code":"26","name":"Mureș","ISO":"MS"},"serial":"101","sex":"female"}');
+        expect(instance.toString()).to.equal('6121212261011');
+        expect(instance.toJSON()).to.deep.equal('{"birthdate":"2012-12-12T00:00:00.000Z","checksum":"1","county":{"code":"26","name":"Mureș","ISO":"MS"},"serial":"101","sex":"female"}');
+        clock.restore();
     });
 });
