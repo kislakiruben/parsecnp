95a3d6d344e6b1fa7f6e933c57d085c0fd909dbb
Ruben Kislaki <ruben@kislaki.net>
2025-10-24 11:58:12 +0200

    fix: explicitly cast string to number

--- src/serialParser.js
+++ src/serialParser.js
@@ -1,3 +1,7 @@
-const serialValidator = (serial) => serial > 0;
+const serialValidator = (serial) => {
+  const num = parseInt(serial, 10);
+
+  return !isNaN(num) && num > 0;
+};
 
 export { serialValidator };
