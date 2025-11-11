e61476388a7ae93ed2c07b7e72045a05d8f66a09
Ruben Kislaki <ruben@kislaki.net>
2025-11-11 15:43:40 +0200

    fix: added upper bounds for serial validator

--- test/serialParser.test.js
+++ test/serialParser.test.js
@@ -3,10 +3,6 @@ import { expect } from "chai";
 import { serialValidator } from "../src/serialParser";
 
 describe(`serial validator`, () => {
-  it(`should return 'true' if serial is bigger than zero`, () => {
-    expect(serialValidator(1)).to.be.true;
-  });
-
   it(`should return 'false' if serial is small or equal to zero`, () => {
     expect(serialValidator(0)).to.be.false;
     expect(serialValidator(-1)).to.be.false;
@@ -15,5 +11,9 @@ describe(`serial validator`, () => {
   it(`should correctly parse if param is a string`, () => {
     expect(serialValidator("000")).to.be.false;
     expect(serialValidator("001")).to.be.true;
+  });
+
+  it(`should return 'false' if serial is bigger than 999`, () => {
+    expect(serialValidator("1000")).to.be.false;
   });
 });
